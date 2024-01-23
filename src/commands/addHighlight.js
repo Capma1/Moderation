8bf968e5294be177de3a76fef71f246a1bd6c357
Capma1 <157472416+Capma1@users.noreply.github.com>
2024-01-23 10:12:14 +0100

    Update addHighlight.js

--- src/commands/addHighlight.js
+++ src/commands/addHighlight.js
@@ -9,19 +9,24 @@ module.exports = {
 		.setName('addhighlight')
 		.setDescription('Create a new highlighted phrase to be notified for (not case sensitive)')
 		.setDMPermission(false)
-		.addStringOption(option => option.setName('phrase').setDescription("The phrase you'd like to highlight").setMaxLength(1_000).setRequired(true)),
+		.addStringOption(option => option.setName('phrase').setDescription("The phrase you'd like to highlight").setMaxLength(1_000).setRequired(true))
+        .addIntegerOption(option => option.setName('duration').setDescription('Cooldown duration in minutes').setRequired(false)), // optional duration parameter
 	async execute(interaction) {
 		await interaction.deferReply();
 		if (!isStaff(interaction, interaction.member, PermissionFlagsBits.ManageMessages)) return sendReply('main', "You're not a moderator, idiot");
 
 		let phrase = interaction.options.getString('phrase').toLowerCase();
+		let duration = interaction.options.getInteger('duration') || 10; // default to 10 minutes if not provided
 
-		let aviURL = interaction.user.avatarURL({ extension: 'png', forceStatic: false, size: 1024 })
-			? interaction.user.avatarURL({ extension: 'png', forceStatic: false, size: 1024 })
-			: interaction.user.defaultAvatarURL;
+		let aviURL = interaction.user.avatarURL({ extension: 'png', forceStatic: false, size: 1024 }) || interaction.user.defaultAvatarURL;
 		let name = interaction.user.username;
 
-		let msgEmbed = new EmbedBuilder().setTitle(`New Highlight Added`).setColor(colors.main).setDescription(`Created highlight for ${phrase}`).setTimestamp().setAuthor({ name: name, iconURL: aviURL });
+		let msgEmbed = new EmbedBuilder()
+			.setTitle(`New Highlight Added`)
+			.setColor(colors.main)
+			.setDescription(`Created highlight for ${phrase}`)
+			.setTimestamp()
+			.setAuthor({ name: name, iconURL: aviURL });
 
 		await prisma.highlight
 			.create({
@@ -29,6 +34,7 @@ module.exports = {
 					phrase: phrase,
 					guildId: interaction.guild.id,
 					userID: interaction.user.id,
+					cooldownDuration: duration, // save the cooldown duration
 				},
 			})
 			.then(r => {
